8c9aa13c3eb172a0a426238ab53286e55330da9a
Wouter Konings <wkonings@f0r3s24.codam.nl>
2023-10-12 18:13:49 +0200

    Made the create_socket function. Removed need for Argument Count to be 3, now uses default port when unspecified. Changed read to recv. Added basic Doxygen. Slightly changed sig_handler

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@
 #include <csignal>
 #include <chrono>
 
-// #define PORT 8080
+#define DEFAULT_PORT 6667
+#define BUFFER_SIZE	1024
 #define HTML_HEADER "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: "
 
 bool escape = false;
@@ -21,23 +22,29 @@ int	error_exit(std::string msg)
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief Just makes ctrl+c quit the program when used with Signal()
+ * 
+ * @param signum automatically gets set by signals.
+ * 
+ * @todo maybe attempt to use the "escape" global to quit?
+ */
 void sig_handler(int signum)
 {
     if (signum == SIGINT) {
-		// exit(1);
 		escape = true;
-		exit(1);
+		// exit(1);
     }
 }
 
-int	main(int ac, char **av)
+/**
+ * @brief Creates a new socket to listen on port
+ * 
+ * @param port Port to bind socket to. Use 0 for default port.
+ * @return int (the socket FD)
+ */
+int	create_socket(int port)
 {
-	signal(SIGINT, sig_handler);
-
-	if (ac != 3)
-		error_exit("so bad ");
-	(void)av;
-
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
 		error_exit("Failed to create socket. errno: ");
@@ -45,7 +52,11 @@ int	main(int ac, char **av)
     sockaddr_in sockaddr;
     sockaddr.sin_family = AF_INET;
     sockaddr.sin_addr.s_addr = INADDR_ANY;
-    sockaddr.sin_port = htons(atoi(av[1]));
+
+	if (port == 0)
+		sockaddr.sin_port = htons(DEFAULT_PORT);
+	else
+		sockaddr.sin_port = htons(port);
 
     if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0)
 		error_exit("Failed to bind to port. errno: ");
@@ -59,15 +70,31 @@ int	main(int ac, char **av)
 	int clientSocket = accept(sockfd, (struct sockaddr*)&clientAddress, &clientAddressLength);
 	if (clientSocket == -1)
 		error_exit("Failed to accept TOS? errno: ");
+	
+	return (clientSocket);
+}
+
+int	main(int ac, char **av)
+{
+	//Signals to make ctrl+c to escape from "Accept" being stuck if no client connects.
+	signal(SIGINT, sig_handler);
+
+	int clientSocket;
+	if (ac == 3)
+		clientSocket = create_socket(atoi(av[1]));
+	else
+		clientSocket = create_socket(0);
 
 	//clientSocket is now the FD to write and read on for one user
-    while (true)
+
+    while (escape == false)
 	{
-		char buffer[1024]; // A buffer to store incoming data
-		ssize_t bytesRead; // Number of bytes read
+		char buffer[BUFFER_SIZE]; // A buffer to store incoming data
+		ssize_t bytesRead = 0; // Number of bytes read
 
 		// Read data from the clientSocket into the buffer
-		bytesRead = read(clientSocket, buffer, sizeof(buffer));
+		if (bytesRead == 0)
+			bytesRead = recv(clientSocket, buffer, BUFFER_SIZE, 0);
 		std::string info(buffer);
 		std::cout << info;
 
@@ -77,12 +104,12 @@ int	main(int ac, char **av)
 		} else if (bytesRead == 0) {
 			// Connection closed by the client
 			std::cout << "Client closed the connection." << std::endl;
+			exit (0); //remove this later, just needed to prevent the giga spam rn
 		} else {
 			// Process and handle the received data
-			buffer[bytesRead] = '\0'; // Null-terminate the received data
+			buffer[bytesRead] = '\0'; // null terminate the received data
 			std::cout << "Received data from client: " << buffer << std::endl;
 		}
 	}
-
     return (0);
 }
