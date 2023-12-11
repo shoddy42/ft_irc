d2db2bef6c14ebc6eb8f1f289b41e8025357f8df
Wouter Konings <wkonings@f1r2s13.codam.nl>
2023-12-11 18:38:39 +0100

    Added print.hpp for colour formatting in stdout messages

---
+++ include/print.hpp
@@ -0,0 +1,22 @@
+#ifndef PRINT_H
+# define PRINT_H
+
+// #Colours
+# define RESET	"\1\33[0m\2"
+# define END	"\1\33[0m\2\3"
+
+# define YELLOW	"\1\33[38;5;220m\2"
+# define BLACK	"\1\33[38;5;232m\2"
+# define ORANGE	"\1\33[38;5;202m\2"
+# define RED	"\1\33[38;5;196m\2"
+# define PINK	"\1\33[38;5;198m\2"
+# define GREEN	"\1\33[38;5;28m\2"
+# define BLUE	"\1\33[38;5;33m\2"
+# define L_BLUE	"\1\33[38;5;69m\2"
+# define D_BLUE	"\1\33[38;5;21m\2"
+# define GREY	"\1\33[38;5;242m\2"
+# define PURPLE	"\1\33[38;5;92m\2"
+# define VIOLET	"\1\33[38;5;183m\2"
+
+#endif
+
