52c5698c44141a41f69b01fffbc0aa6820a870d6
Augustine udo Uzochukwu <auzochuk@f1r1s9.codam.nl>
2024-02-08 15:36:09 +0100

    mode parsing

--- src/commands/mode.cpp
+++ src/commands/mode.cpp
@@ -1,8 +1,33 @@
 #include "../../include/Command.hpp"
 
+static void parse_arguments(std::string args)
+{
+	static std::string modes = "itko";
+
+	if (args.empty() || args.length() > 2)
+		return; //or error
+	if(!args[0] != '-' || args[0] != '+')
+	{
+		bool found = false;
+		for(int i = 0; i < 3; i++)
+		{
+			if (args[1] == modes[i])
+				found = true;
+		}
+		if (found == false)
+			std::cout << "Bad" << std::endl;
+			//error or something
+	}
+
+}
+
 void	Command::mode(void)
 {
 	std::cout << ORANGE << "MODE called with: [" << _arguments[1] << "]\n" << RESET;
 
+	_a
+
 	
 }
+
+//	while(args.find(modes[i])= )
