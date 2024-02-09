2eb29cb174db70f3574aff9267a6521a5e0f7d8d
shoddy42 <wkonings@student.codam.nl>
2024-02-09 16:40:16 +0100

    Cleaned up server file

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: wkonings <wkonings@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/19 13:21:59 by wkonings      #+#    #+#                 */
-/*   Updated: 2023/12/13 16:52:30 by shoddy        ########   odam.nl         */
+/*   Updated: 2024/02/09 16:35:21 by shoddy        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,98 +20,78 @@
 # define MAX_CLIENTS  512
 # define DEFAULT_PORT 6667
 # define POLL_TIMEOUT 2000 //in ms
-# define BUFFER_SIZE  1024
+# define BUFFER_SIZE  1024 //recommended to keep above 512.
 
 # define INVALID_FD   -1
 
 	//  Dependencies    //
-
-# include <string>
-# include <sstream>
-# include <strings.h>
-# include <poll.h>
-# include <sys/socket.h>
-# include <netinet/in.h>
-# include <list>
-# include <fcntl.h>
-# include <unistd.h>
-
 # include "../include/User.hpp"
 # include "../include/Command.hpp"
 # include "../include/Channel.hpp"
 # include "../include/print.hpp"
 
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <strings.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <poll.h>
+# include <sstream>
+# include <string>
+# include <list>
+
+# include <unordered_map>
 # include <vector>
 # include <map>
-# include <unordered_map>
 
 int			error_exit(std::string error_msg);
 int			guard(int n, std::string error_msg);
 int			parsePort(std::string portNumber);
 std::string	parsePassword(std::string password);
 
-// // commands to fill the command_map
-// int quit(std::vector<std::string> command);
-// int join(std::vector<std::string> command);
-// int privmsg(std::vector<std::string> command);
-// int nick(std::vector<std::string> command);
-
 class Channel;
 
 class Server
 {
-	public:
-	//	constructors
+	private:
+		int			_listen_socket;
+		std::string _password;
 
+	public:
+	//	variables
+		std::vector<pollfd>	pollfds;  // pollfds[0] is the servers listening client. all others are the users. 
+		std::list<User>		users;    // users[0] is a dummy user in case no user is found
+		std::list<Channel>  channels; // channels[0] is a dummy channel in case no channel is found
+
+	//	constructors
 		Server(void);
 		Server(const Server &src);
 		~Server(void);
 		Server &operator=(Server const &src);
 
 	//	methods
-
-		void	serve(void);
-		void	start(int port, std::string password);
-
-	// getters/setters
-
-		std::string	&get_password(void); //todo: make const?
-		void		add_user(int sock);
-		User		&get_user(int sock);
-		User		&get_user(std::string name);
-		Channel		&get_channel(const std::string name);
-		void		add_channel();
-
-	// networking
-
-		void	accept_new_connection(void);
-		void	socket_cleanup(int sock);
-		void	shutdown(void);
-		void	delete_user(User &user);
-
-
-	// commands
-
+		void		start(int port, std::string password);
+		void		serve(void);
 		std::string	receive(int sock);
 		void		respond(User &user);
 		void		create_command(std::string buffer, User &caller);
 
-		
-	//	variables
+	// networking
+		void		accept_new_connection(void);
+		void		socket_cleanup(int sock);
+		void		shutdown(void);
+		void		delete_user(User &user);
 
-		// pollfds[0] is the servers listening client. all others are the users. 
-		std::vector<pollfd>	pollfds;
-		// users[0] is a dummy user in case no user is found
-		std::list<User>		users;
-		// channels[0] is a dummy channel in case no channel is found
-		std::list<Channel>  channels;
-		
-	private:
-		int	_listen_socket;
-		std::string _password;
+	// getters/setters
+		void		add_channel(std::string name);
+		void		add_user(int sock);
+
+		Channel		&get_channel(const std::string name);
+		User		&get_user(std::string name);
+		User		&get_user(int sock);
+		std::string	&get_password(void);
 
 
 };
-
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: wkonings <wkonings@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/19 13:21:51 by wkonings      #+#    #+#                 */
-/*   Updated: 2023/12/16 14:35:20 by shoddy        ########   odam.nl         */
+/*   Updated: 2024/02/09 16:39:43 by shoddy        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-
 Server::Server(void)
 {
-
 }
 
 Server::Server(const Server &src)
@@ -29,16 +27,14 @@ Server::Server(const Server &src)
 		*this = src;
 }
 
-
-
 /*
 ** -------------------------------- DESTRUCTOR --------------------------------
 */
 
 Server::~Server(void)
 {
+	// this->shutdown();
 }
-
 
 /*
 ** --------------------------------- OVERLOAD ---------------------------------
@@ -48,69 +44,17 @@ Server &Server::operator=(Server const &src)
 {
 	if (this == &src)
 		return (*this);
+	pollfds = src.pollfds;
+	users = src.users;
+	channels = src.channels;
+	_listen_socket = src._listen_socket;
+	_password = src._password;
 	return (*this);
 }
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
-
-std::string	&Server::get_password(void)
-{
-	return (_password);
-}
-
-
-/**
- * @brief Creates a new pollfd, configures it to check for POLLIN and POLLOUT events. Then creates a new user with this data
- * 
- * @param sock 
- */
-void Server::add_user(int sock)
-{
-	pollfd new_fd;
-	new_fd.fd = sock;
-	new_fd.events = POLLIN | POLLOUT;
-	new_fd.revents = 0;
-	pollfds.push_back(new_fd);
-
-	User new_user(sock);
-	users.push_back(new_user);
-}
-
-User &Server::get_user(int sock)
-{
-	//next to skip dummy at start users
-	for (std::list<User>::iterator user = std::next(users.begin()); user != users.end(); user++)
-		if (user->get_socket() == sock)
-			return (*user);
-	std::cout << RED << "No user is connected to sock" << sock << "\n" << RESET;
-	return (*users.begin());
-	// throw std::runtime_error("User not found");
-}
-
-User &Server::get_user(std::string name)
-{
-	// for (std::list<User>::iterator user = std::next(users.begin()); user != users.end(); user++)
-	for (std::list<User>::iterator user = users.begin(); user != users.end(); user++)
-		if (user->get_username() == name)
-			return (*user);
-	std::cout << RED << "No user " << name << " found.\n" << RESET;
-	return (*users.begin());
-	// throw std::runtime_error("User not found");
-}
-
-Channel	&Server::get_channel(const std::string name)
-{
-	// return (*channels.begin());
-	for (std::list<Channel>::iterator channel = channels.begin(); channel != channels.end(); channel++)
-		if (channel->get_name() == name)
-			return (*channel);
-	std::cout << RED << "No channel " << name << " found.\n" << RESET;
-	return (*channels.begin());
-	// throw std::runtime_error("Channel not found");
-}
-
 
 /**
  * @brief Creates a new socket to listen on port. Configures server.poll[0] to be this new socket.
@@ -128,8 +72,9 @@ void	Server::start(int port, std::string password)
     sock_address.sin_family = AF_INET;
     sock_address.sin_addr.s_addr = INADDR_ANY;
 
-    // guard(bind(_listen_socket, (struct sockaddr*)&sock_address, sizeof(sock_address)), "failed to bind to port. errno: ");
-    guard(bind(_listen_socket, (struct sockaddr*)&sock_address, sizeof(sockaddr_in)), "failed to bind to port. errno: ");
+	int opt = 1;
+    guard(bind(_listen_socket, (struct sockaddr*)&sock_address, sizeof(sockaddr_in)), "Failed to bind to port. errno: ");
+	guard(setsockopt(_listen_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)), "Failed to set socket to reusable");
 	guard(listen(_listen_socket, MAX_CLIENTS), "Failed to listen on socket. errno: ");
    
 	pollfd listen_socket;
@@ -149,12 +94,119 @@ void	Server::start(int port, std::string password)
 	_password = password;
 }
 
+/**
+ * @brief The main loop. Polls all sockets. Then receives and responds when they are ready.
+ * 
+ */
+void	Server::serve(void)
+{
+	//Poll every socket we have.
+	//Check all open sockets, to see if any have written to us.
+	poll(pollfds.data(), pollfds.size(), POLL_TIMEOUT);
+	accept_new_connection();
+	for (size_t i = 1; i < pollfds.size(); i++)
+	{
+		if (pollfds[i].revents == 0) //client hasnt done anything
+			continue;
+		if (pollfds[i].revents & POLLHUP) //client disconnected
+		{
+			std::cout << "Socket " << i << " hung up." << std::endl;
+			delete_user(get_user(pollfds[i].fd));
+		}
+		if (pollfds[i].revents & POLLIN) //client sent server a message
+		{
+			std::string message;
+			message = receive(i);
+			create_command(message, get_user(pollfds[i].fd));
+		}
+		if (pollfds[i].revents & POLLOUT) //client is ready for a response
+		{
+			respond(get_user(pollfds[i].fd));
+		}
+	}
+}
+
+std::string	Server::receive(int sock)
+{
+	std::cout << "receiving from socket [" << pollfds[sock].fd << "], at position (" << sock << ") AKA " << get_user(pollfds[sock].fd).get_username() << std::endl;
+	char	buffer[BUFFER_SIZE];
+	ssize_t	bytes_read;
+
+	bzero(buffer, BUFFER_SIZE);
+	bytes_read = recv(pollfds[sock].fd, buffer, BUFFER_SIZE, 0);
+	if (bytes_read == -1)	  // Recv failed
+	{
+		perror("recv");
+		delete_user(get_user(pollfds[sock].fd));
+	}
+	else if (bytes_read == 0) // Connection closed by the client //might not be needed, POLLHUP should already catch
+	{
+		std::cout << "Client closed the connection." << std::endl;
+		delete_user(get_user(pollfds[sock].fd));
+	} 
+	else  				      // Actually received a message
+	{
+		std::string data(buffer);
+		return (data);
+	}
+	return ("");
+}
+
+void	Server::respond(User &user)
+{
+	while (user.check_response() > 0)
+	{
+		std::string response = user.give_response();
+		if (response.empty())
+			break;
+		std::cout << "Sending Packet: " << YELLOW << response << RESET << " to client " << user.get_socket() << std::endl;
+		response += "\r\n";
+		send(user.get_socket(), response.c_str(), response.length(), 0);
+
+		if (response == "462 :Unauthorized command (already registered)\r\n")
+		{
+			std::cout << "Registered user getting booted!\n";
+			std::string disconnect_msg = "ERROR :You have been kicked from the server (Reason: Account Already Registered)." ;
+			send(user.get_socket(), disconnect_msg.c_str(), disconnect_msg.length(), 0);
+			delete_user(user);
+		}
+		else if (response == "464 * :Password incorrect!\r\n")
+		{
+			std::cout << "Unregistered user getting booted!\n";
+			std::string disconnect_msg = "ERROR :You have been kicked from the server (Reason: Invalid password)." ;
+			send(user.get_socket(), disconnect_msg.c_str(), disconnect_msg.length(), 0);
+			delete_user(user);
+		}
+	}
+}
+
+void	Server::create_command(std::string buffer, User &caller)
+{
+	if (buffer.empty())
+		return;
+
+	std::stringstream ss(buffer);
+	std::string line;
+	while (std::getline(ss, line, '\n'))
+	{
+		Command command(*this, caller);
+		std::cout << BLUE << line << RESET << std::endl;
+		if (!line.empty())
+		{
+			std::istringstream split(line);
+			std::string token;
+			while (split >> token)
+				command.add_argument(token);
+		}
+		command.execute();
+	}
+}
+
 void	Server::shutdown(void)
 {
 	for (size_t i = 0; i < pollfds.size(); i++)
 		if (pollfds[i].fd > 0)
-			close(pollfds[i].fd);
-	exit(0);
+			socket_cleanup(pollfds[i].fd);
 }
 
 /**
@@ -166,13 +218,10 @@ void	Server::accept_new_connection(void)
 	if (pollfds[0].revents & POLLIN)
 	{
 		std::cout << "New connection found. CONNECTING" << std::endl;
-		//possible here to steal client information with sockaddr instead of nullptr.
 		int client_socket = guard(accept(pollfds[0].fd, nullptr, nullptr), "Failed to accept socket. errno: ");
-		std::cout << "TEST: " << client_socket << std::endl;
 		guard(fcntl(client_socket, F_SETFL, O_NONBLOCK), "Failed to set socket to non-blocking. errno: ");
 		std::cout << "client socket created at: " << client_socket << std::endl;
 		this->add_user(client_socket);
-		// send(client_socket, "ERROR :Password required\r\n", 6, 0);
 	}
 }
 
@@ -205,18 +254,12 @@ void	Server::socket_cleanup(int sock)
 
 void	Server::delete_user(User &user_to_delete)
 {
-	//loop over all channels and delete the user.
-	// for (Channel channel : channels)
 	for (std::list<Channel>::iterator channel = channels.begin(); channel != channels.end(); channel++)
 	{
 		std::cout << PURPLE << "Looking for user in " << channel->get_name() << " channel\n" << RESET;
 		channel->remove_user(user_to_delete);
 	}
-	
-	//clean up socket
 	socket_cleanup(user_to_delete.get_socket());
-
-	//remove user
 	for(std::list<User>::iterator user = users.begin(); user != users.end(); user++)
 	{
 		if (user->get_socket() == user_to_delete.get_socket())
@@ -227,118 +270,60 @@ void	Server::delete_user(User &user_to_delete)
 	}
 }
 
-//todo: Make this function loop until CLRF (\r\n) is found, instead of assuming BUFFER_SIZE is enough for the whole packet
-std::string	Server::receive(int sock)
-{
-	std::cout << "receiving from socket [" << pollfds[sock].fd << "], at position (" << sock << ") AKA " << get_user(pollfds[sock].fd).get_username() << std::endl;
-	char	buffer[BUFFER_SIZE];
-	ssize_t	bytes_read;
-
-	bzero(buffer, BUFFER_SIZE);
-	bytes_read = recv(pollfds[sock].fd, buffer, BUFFER_SIZE, 0);
-	if (bytes_read == -1)	  // Recv failed
-	{
-		perror("recv");
-		// socket_cleanup(pollfds[sock].fd);
-	}
-	else if (bytes_read == 0) // Connection closed by the client //might not be needed, POLLHUP should already catch
-	{
-		std::cout << "Client closed the connection." << std::endl;
-		// socket_cleanup(pollfds[sock].fd);
-		delete_user(get_user(pollfds[sock].fd));
-	} 
-	else  				      // Actually received a message
-	{
-		std::string data(buffer);
-		return (data);
-	}
-	return ("");
-}
-
-void	Server::respond(User &user)
-{
-	while (user.check_response() > 0)
-	{
-		std::string response = user.give_response();
-		if (response.empty()) //shouldn't be needed but safety /shrug
-			break;
-		std::cout << "Sending Packet: " << YELLOW << response << RESET << " to client " << user.get_socket() << std::endl;
-		response += "\r\n";
-		send(user.get_socket(), response.c_str(), response.length(), 0);
-
-		if (response == "462 :Unauthorized command (already registered)\r\n")
-		{
-			std::cout << "Registered user getting booted!\n";
-			std::string disconnect_msg = "ERROR :You have been kicked from the server (Reason: Account Already Registered)." ;
-			send(user.get_socket(), disconnect_msg.c_str(), disconnect_msg.length(), 0);
-			delete_user(user);
-		}
-		else if (response == "464 * :Password incorrect!\r\n")
-		{
-			std::cout << "Unregistered user getting booted!\n";
-			std::string disconnect_msg = "ERROR :You have been kicked from the server (Reason: Invalid password)." ;
-			send(user.get_socket(), disconnect_msg.c_str(), disconnect_msg.length(), 0);
-			delete_user(user);
-		}
-	}
-}
-
-void	Server::serve(void)
-{
-	//Poll every socket we have.
-	//Check all open sockets, to see if any have written to us.
-	guard(poll(pollfds.data(), pollfds.size(), POLL_TIMEOUT), "poll failed. errno: ");
-
-	accept_new_connection();
-	for (size_t i = 1; i < pollfds.size(); i++)
-	{
-		if (pollfds[i].revents == 0) //client hasnt done anything
-			continue;
-		if (pollfds[i].revents & POLLHUP) //client disconnected
-		{
-			std::cout << "Socket " << i << " hung up." << std::endl;
-			delete_user(get_user(pollfds[i].fd));
-		}
-		if (pollfds[i].revents & POLLIN) //client sent server a message
-		{
-			std::string message;
-			message = receive(i);
-			create_command(message, get_user(pollfds[i].fd));
-		}
-		if (pollfds[i].revents & POLLOUT) //client is ready for a response
-		{
-			respond(get_user(pollfds[i].fd));
-		}
-	}
-}
-
-void	Server::create_command(std::string buffer, User &caller)
-{
-	if (buffer.empty())
-		return;
-
-	std::stringstream ss(buffer);
-
-	std::string line;
-	while (std::getline(ss, line, '\n'))
-	{
-		Command command(*this, caller);
-		std::cout << BLUE << line << RESET << std::endl;
-		if (!line.empty()) //might be useless? might not be good enough? shrug
-		{
-			std::istringstream split(line);
-			std::string token;
-			while (split >> token)
-				command.add_argument(token);
-		}
-		command.execute();
-	}
-}
-
-
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
 
+void Server::add_channel(std::string name)
+{
+	// if (name[0] != '#')
+	// 	return;
+	Channel new_channel(name, *this);
+	channels.push_back(new_channel);
+}
+
+void Server::add_user(int sock)
+{
+	pollfd new_fd;
+	new_fd.fd = sock;
+	new_fd.events = POLLIN | POLLOUT;
+	new_fd.revents = 0;
+	pollfds.push_back(new_fd);
+
+	User new_user(sock);
+	users.push_back(new_user);
+}
+
+Channel	&Server::get_channel(const std::string name)
+{
+	for (std::list<Channel>::iterator channel = channels.begin(); channel != channels.end(); channel++)
+		if (channel->get_name() == name)
+			return (*channel);
+	std::cout << RED << "No channel " << name << " found." << std::endl << RESET;
+	return (*channels.begin());
+}
+
+User &Server::get_user(std::string name)
+{
+	for (std::list<User>::iterator user = std::next(users.begin()); user != users.end(); user++)
+		if (user->get_username() == name)
+			return (*user);
+	std::cout << RED << "No user " << name << " found." << std::endl << RESET;
+	return (*users.begin());
+}
+
+User &Server::get_user(int sock)
+{
+	for (std::list<User>::iterator user = std::next(users.begin()); user != users.end(); user++)
+		if (user->get_socket() == sock)
+			return (*user);
+	std::cout << RED << "No user is connected to sock" << sock << std::endl << RESET;
+	return (*users.begin());
+}
+
+std::string	&Server::get_password(void)
+{
+	return (_password);
+}
 
 /* ************************************************************************** */
