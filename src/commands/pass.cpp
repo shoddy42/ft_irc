2aa9163d14291e894b91d712efb43f031692ecec
shoddy42 <wkonings@student.codam.nl>
2024-05-14 18:52:58 +0200

    password accept colon as first charcter, server delay to 50ms

--- src/commands/pass.cpp
+++ src/commands/pass.cpp
@@ -6,7 +6,7 @@
 /*   By: shoddy <shoddy@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/13 17:23:24 by shoddy        #+#    #+#                 */
-/*   Updated: 2024/03/05 15:45:04 by shoddy        ########   odam.nl         */
+/*   Updated: 2024/05/14 18:50:51 by shoddy        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	Command::pass(void)
 {
+	std::string password = _arguments[1];
+	if (password.length() > 1 && password[0] == ':')
+		password = password.erase(0, 1);
+
 	std::cout << "Server PASS [" << _server.get_password() << "]\n";
-	if (_arguments[1] != _server.get_password())
+	if (password != _server.get_password())
 	{
 		std::string wrong_pass = "464 * :Password incorrect!";
 		_caller.add_response(wrong_pass);
